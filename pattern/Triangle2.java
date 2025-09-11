83d5b5f9afc25b0d0dd21ab7880c4ec9b5b90eb6
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-09-11 19:11:50 +0530

    dsa with java

---
+++ pattern/Triangle2.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class Triangle2 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = n; i>=1; i--){
+            for(int j = 1; j <= i; j++){
+                System.err.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
