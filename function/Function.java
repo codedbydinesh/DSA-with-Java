066121ba3f8cd1925da0c3e1b19c8af0302d5067
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-09-16 20:30:25 +0530

    Add Function class to print user-provided name

---
+++ function/Function.java
@@ -0,0 +1,17 @@
+package function;
+
+import java.util.Scanner;
+
+public class Function {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return ;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);  // function call
+
+        sc.close();
+    }
+}
