a81b4d4c7eb2cb68d0fca0c70751c7cda3fd7fa6
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-09-23 22:33:29 +0530

    add selection array

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,27 @@
+package sorting;
+
+// selection sort
+public class SelectionSort {
+    public static void printArray(int arr[]){
+        for(int i =0 ; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+         int arr[] = {7,6,8,3,1,2,5};
+
+         for(int i = 0; i < arr.length - 1; i++){
+            int smallest = i;
+            for(int j = i+1; j < arr.length; j++){
+                if (arr[smallest] > arr[j]) {
+                    
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+         }
+         printArray(arr);
+    }
+}
