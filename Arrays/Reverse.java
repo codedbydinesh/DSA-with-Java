acb05b972f48f4c2c1cdb218d2bb5266e732526e
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-10-05 22:03:13 +0530

    Add Reverse array implementation

---
+++ Arrays/Reverse.java
@@ -0,0 +1,25 @@
+package arrays;
+
+
+// reverse an Array
+public class Reverse {
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i< arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {9,8,7,6,5};
+        int len = arr.length-1;
+        for (int i = 0; i < arr.length / 2; i++) {
+            int front = i;
+            int last = arr.length - 1 - i;
+
+            int temp = arr[front];
+            arr[front] = arr[last];
+            arr[last] = temp;
+        }
+        printArr(arr);
+    }
+}
